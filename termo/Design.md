5237d952fc1c8ae55007444a1083308c426d3e82
Daniel Nogueira <daniel.nogueira.dacosta@gmail.com>
2023-12-07 17:33:07 -0300

    arquivo para nortear as tomadas de decisão/design

---
+++ termo/Design.md
@@ -0,0 +1,15 @@
+# Design da aplicação
+
+## Descrição
+
+## Problemas e Soluções
+
+## Requisitos
+
+## Casos de uso
+
+## Diagrama de classes
+
+## Arquitetura
+
+## Tecnologias
--- termo/src/main/java/com/realbetis/termo/domain/Pais.java
+++ termo/src/main/java/com/realbetis/termo/domain/Pais.java
@@ -2,16 +2,13 @@ package com.realbetis.termo.domain;
 
 import lombok.Data;
 
-import java.util.HashMap;
 import java.util.List;
-
-import java.util.Map;
 
 @Data
 public class Pais {
     private String nome;
     private String isoCode;
-    private String moeda;
+    private List<String> moedas;
     private Coordenada coordenada;
     private boolean litoraneo;
     private List<String> fronteiras;
